c9adb77a24fdbe1c60739ec25dce5867337d1853
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2025-06-14 08:30:10 +0300

    titles

--- public/index.jsx
+++ public/index.jsx
@@ -46,20 +46,9 @@ function App({ years, levels, categories, authors, statuses }) {
           <Checkboxes name="Спікер" selected={selectedAuthors} options={authors} toggle={toggleAuthors} />
           <Checkboxes name="Статус" selected={selectedStatuses} options={statuses} toggle={toggleStatuses} />
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8">
           {items.map((item) => (
-            <a key={item.id} className="max-w-[480px]" href={item.telegram} target="_blank" rel="nofollow">
-              <img src={`https://img.youtube.com/vi/${item.id}/0.jpg`} alt={item.title} />
-              <div className="bg-black text-neutral-500 p-2 text-center">
-                <span>{item.year}</span>
-                <span className="text-neutral-700 mx-2">|</span>
-                <span>{item.categories.join(", ")}</span>
-                <span className="text-neutral-700 mx-2">|</span>
-                <span>{item.levels.join(", ")}</span>
-                <span className="text-neutral-700 mx-2">|</span>
-                <span>{item.authors.join(", ")}</span>
-              </div>
-            </a>
+            <Video key={item.id} {...item} />
           ))}
         </div>
       </div>
@@ -95,6 +84,33 @@ function useCheckboxes(key) {
   };
 
   return [selected, toggle];
+}
+
+function Video({ id, title, year, categories, levels, authors, telegram }) {
+  return (
+    <a className="max-w-[480px]" href={telegram} target="_blank" rel="nofollow">
+      <div className="aspect-video bg-center rounded" style={{ backgroundImage: `url("https://img.youtube.com/vi/${id}/0.jpg")` }} />
+      <div className="pt-2 text-xl font-bold">{title}</div>
+      <div className="pt-2 flex gap-2 flex-wrap">
+        <div className="flex items-center gap-2">
+          <Calendar />
+          <span>{year}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Bookmark />
+          <span>{categories.join(", ")}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <DumpBell />
+          <span>{levels.join(", ")}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <UserRounded />
+          <span>{authors.join(", ")}</span>
+        </div>
+      </div>
+    </a>
+  );
 }
 
 function Checkboxes({ name, selected, options, toggle }) {
@@ -136,6 +152,47 @@ function Search() {
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="m21 21-4.34-4.34" />
       <circle cx="11" cy="11" r="8" />
+    </svg>
+  );
+}
+
+function Calendar() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar1-icon lucide-calendar-1">
+      <path d="M11 14h1v4" />
+      <path d="M16 2v4" />
+      <path d="M3 10h18" />
+      <path d="M8 2v4" />
+      <rect x="3" y="4" width="18" height="18" rx="2" />
+    </svg>
+  );
+}
+
+function Bookmark() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bookmark-icon lucide-bookmark">
+      <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
+    </svg>
+  );
+}
+
+function UserRounded() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round-icon lucide-user-round">
+      <circle cx="12" cy="8" r="5" />
+      <path d="M20 21a8 8 0 0 0-16 0" />
+    </svg>
+  );
+}
+
+function DumpBell() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-dumbbell-icon lucide-dumbbell">
+      <path d="M17.596 12.768a2 2 0 1 0 2.829-2.829l-1.768-1.767a2 2 0 0 0 2.828-2.829l-2.828-2.828a2 2 0 0 0-2.829 2.828l-1.767-1.768a2 2 0 1 0-2.829 2.829z" />
+      <path d="m2.5 21.5 1.4-1.4" />
+      <path d="m20.1 3.9 1.4-1.4" />
+      <path d="M5.343 21.485a2 2 0 1 0 2.829-2.828l1.767 1.768a2 2 0 1 0 2.829-2.829l-6.364-6.364a2 2 0 1 0-2.829 2.829l1.768 1.767a2 2 0 0 0-2.828 2.829z" />
+      <path d="m9.6 14.4 4.8-4.8" />
     </svg>
   );
 }
